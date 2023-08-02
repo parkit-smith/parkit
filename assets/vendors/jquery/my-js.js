dab259634b5e853b9c6f42d8ef7d0e6c6fd8b2af
Laura Edwards <lledwards@smith.edu>
2023-08-02 15:47:00 -0400

    debug

--- assets/vendors/jquery/my-js.js
+++ assets/vendors/jquery/my-js.js
@@ -7,6 +7,15 @@ $(document).ready(function(){
     });
 });
 
+$(document).ready(function(){
+  $(window).scroll(function(){
+   if ($(window).scrollTop() > 30) {
+    $("#logo").css('color', 'black');
+    } else {
+    $("#logo").css('color', 'white')}
+    });
+});
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
